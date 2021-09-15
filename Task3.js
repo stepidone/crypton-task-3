dd92114dee9e786745ece3dbcaf968090b33dcfe
stepidone <notahumangarbage@gmail.com>
2021-09-16 03:10:11 +0700

    comments del

--- Task3.js
+++ Task3.js
@@ -1,26 +1,5 @@
 var arr = [88, 494, 4, 84, 944, 449, 48, 8, 7, 8, 6, 0, 0, 9, 1, 4]
 
-// arr.sort(function (a, b) {
-//     if (a != 8 && b == 8) {
-//         return -1
-//     }
-//     if (a != 9 && a != 8 && b == 9) {
-//         return -1
-//     }
-//     if (a != 6 && a != 8 && a != 9 && b == 6) {
-//         return -1
-//     }
-//     if (a != 4 && a != 8 && a != 9 && a != 6 && b == 4) {
-//         return -1
-//     }
-//     if (a != 0 && a != 8 && a != 9 && a != 6 && a != 4 && b == 0) {
-//         return -1
-//     }
-//     if (a != 8 && a != 9 && a != 6 && a != 4 && a != 0) {
-//         return a - b
-//     }
-// })
-
 arr.sort(function(a, b){
     var digits = a.toString().split('');
     var aaa = digits.map(Number)
